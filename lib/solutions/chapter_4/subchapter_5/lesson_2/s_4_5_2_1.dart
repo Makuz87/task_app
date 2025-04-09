43e947e146b98b171bc2896674961de7c4c0f1b5
Markus Willmann <markuswillmann87@gmail.com>
2025-04-09 11:28:47 +0200

    4_5_3_6 korregiert

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_1.dart
@@ -4,6 +4,17 @@ class S4521 extends StatelessWidget {
   const S4521({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyCenterWidget();
+  }
+}
+
+class MyCenterWidget extends StatelessWidget {
+  const MyCenterWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Hello World"),
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -16,16 +16,32 @@ class EinkaufsListe extends StatelessWidget {
     List<Product> products = [
       Product("T-Shirt", 4.99),
       Product("Hose", 10.99),
-      Product("Unterwäsche", 1.99)
+      Product("Unterwäsche", 1.99),
+      Product("Socken", 0.99),
+      Product("Cap", 11.99),
     ];
     return Container(
-      height: 200,
+      // height: 200,
       child: ListView(
+        shrinkWrap: true,
         children: <Widget>[
-          ListTile(leading: Icon(Icons.boy), title: Text(products[0].name)),
-          ListTile(leading: Icon(Icons.south), title: Text(products[1].name)),
           ListTile(
-              leading: Icon(Icons.add_outlined), title: Text(products[2].name)),
+              leading: Icon(Icons.shopping_cart),
+              title: Text(products[0].name)),
+          ListTile(
+              leading: Icon(Icons.shopping_cart),
+              title: Text(products[1].name)),
+          ListTile(
+              leading: Icon(Icons.shopping_cart),
+              title: Text(products[2].name)),
+          ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text(products[3].name),
+          ),
+          ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text(products[4].name),
+          )
         ],
       ),
     );
